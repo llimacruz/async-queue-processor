00af0f5de38277eaf58a3c8a40475bffbcee3ebe
Leonardo de Lima Cruz <leonardo.cruz@outlook.com>
2017-07-09 17:47:50 -0300

    Improving example and logs

--- using-example.js
+++ using-example.js
@@ -3,10 +3,8 @@ var queue = require('./index')
 var teste = 0
 
 function whatProcess(callback) {
-    // console.log('getting items that must be pushed into queue')
-    // var items = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     var items = []
-    for(var i=0; i<1000; i++) {
+    for(var i=0; i<100; i++) {
         items.push(i);
     }
     if (teste % 3 !== 0) {
